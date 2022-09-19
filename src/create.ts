ef7ef71a02f144c30cc378e9310daa5d55a9bd3d
Maxim Savenkov <max_savenkov@mail.ru>
2022-09-19 17:16:35 +0200

    Rebase of file system

--- src/create.ts
+++ src/create.ts
@@ -2,7 +2,7 @@ import { Select, Input, Confirm, prompt, Checkbox } from "https://deno.land/x/cl
 
 import { ConfigFile, writeConfig } from "./file.ts";
 import { printCreateHelpMenu } from "./help.ts";
-import { shs } from "./utils.ts";
+import { sh, shs } from "./utils.ts";
 
 export async function createProject(args: string[]) {
     if (args.length > 1 && args[1] == "--help") {
@@ -88,6 +88,8 @@ export async function createProject(args: string[]) {
     if (setup.gitLink == null) {
         setup.gitLink = "";
     }
+    const commit = setup.commit;
+    setup.commit = undefined;
     const config = setup as ConfigFile;
     await writeConfig(config);
     if (config.git) {
@@ -95,34 +97,7 @@ export async function createProject(args: string[]) {
             `git init`,
             `git remote add origin ${config.gitLink}`,
             `git branch -M main`
-        ])
+        ]);
     }
-    let packageFiles: string[] = [];
-    let sourceDir: string[] = [];
-    switch (config.language) {
-        case "Java":
-            packageFiles = [
-                `curl 'https://files.factoryisland.net/${config.framework}/pom.xml' -o pom.xml`,
-                `curl 'https://files.factoryisland.net/${config.framework}/main.java' -o src/main/java/Main.java`
-            ];
-            sourceDir = [
-                `mkdir src`,
-                `mkdir src/main`,
-                `mkdir src/main/java`,
-            ]
-            break;
-        default:
-            break;
-    }
-    await shs(packageFiles);
-    await shs(sourceDir);
-    await shs([
-        `curl 'https://files.factoryisland.net/${config.framework}/package.dat' -o .package`,
-        `curl 'https://files.factoryisland.net/${config.framework}/libs.tar.gz' -o libs.tar.gz`,
-        `curl 'https://files.factoryisland.net/${config.framework}/out.tar.gz' -o out.tar.gz`,
-        `tar -xzf *.tar.gz`,
-        `rm *.tar.gz`,
-        `mkdir src`,
-        `mkdir src/`
-    ])
+    await sh(`curl 'https://files.factoryisland.net/${config.framework}/setup.sh' -o setup.sh | sh`);
 }
